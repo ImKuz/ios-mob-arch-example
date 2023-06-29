b15815eb56e49dc0e93b99e4d1bde7aa5c0c7b18
Mikhail Kuzmin <mkuzmin231@gmail.com>
2023-06-30 00:51:47 +0700

    Update dependencies

--- Sources/Tuist/ProjectDescriptionHelpers/ExternalDependencies.swift
+++ Sources/Tuist/ProjectDescriptionHelpers/ExternalDependencies.swift
@@ -4,11 +4,11 @@ public let dependencyList: [ExternalDependency] = [
     .init(
         name: "Swinject",
         url: "https://github.com/Swinject/Swinject",
-        requirments: .upToNextMinor(from: "2.8.3")
+        requirments: .upToNextMinor(from: "2.8.0")
     ),
     .init(
         name: "ComposableArchitecture",
         url: "https://github.com/pointfreeco/swift-composable-architecture",
-        requirments: .upToNextMinor(from: "0.52.0")
+        requirments: .upToNextMinor(from: "0.55.0")
     ),
 ]
